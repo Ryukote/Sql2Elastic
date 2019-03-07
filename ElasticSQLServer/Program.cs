f61e974d4bac0682307b2da6636ea89560427707
Ryukote <ryukote@outlook.com>
2019-03-07 21:45:37 +0100

    Project setup initialization
    Adding project and solution, adding CoreGenerics Nuget package.

---
+++ ElasticSQLServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElasticSQLServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
